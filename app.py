69e09d3599c38c59c88934205467c203f87bf80a
Pranav Kushare <66110778+Pranav082001@users.noreply.github.com>
2023-08-06 22:51:24 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from tools.fetch_stock_info import Anazlyze_stock
 st.title("Stock Analysis bot")
 st.write("This bot scraps and gathers real time stock realted information and analyzes it using LLM")
 
-#streamlit make one textbox
 query = st.text_input('Input your investment related query:') 
 
 Enter=st.button("Enter")
