37359bdb640b137eb963d586195c933547ef67d0
Pranav Kushare <66110778+Pranav082001@users.noreply.github.com>
2023-08-06 22:50:48 +0530

    Added streamlit app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import tools
+from tools.fetch_stock_info import Anazlyze_stock
+
+st.title("Stock Analysis bot")
+st.write("This bot scraps and gathers real time stock realted information and analyzes it using LLM")
+
+#streamlit make one textbox
+query = st.text_input('Input your investment related query:') 
+
+Enter=st.button("Enter")
+clear=st.button("Clear")
+
+if clear:
+    print(clear)
+    st.markdown(' ')
+
+if Enter:
+    import time
+    with st.spinner('Gathering all required information and analyzing. Be patient!!!!!'):
+        out=Anazlyze_stock(query)
+    st.success('Done!')
+    st.write(out)
+
+
---
+++ tools/fetch_stock_info.py
@@ -0,0 +1,150 @@
+import json
+import time
+from bs4 import BeautifulSoup
+import re
+import requests
+
+from langchain.llms import OpenAI
+from langchain.agents import load_tools, AgentType, Tool, initialize_agent
+import yfinance as yf
+
+import openai
+import warnings
+import os
+warnings.filterwarnings("ignore")
+
+os.environ["OPENAI_API_KEY"] = "Enter your API key here"
+
+
+llm=OpenAI(temperature=0,
+           model_name="gpt-3.5-turbo-16k-0613")
+
+
+# Fetch stock data from Yahoo Finance
+def get_stock_price(ticker,history=5):
+    # time.sleep(4) #To avoid rate limit error
+    if "." in ticker:
+        ticker=ticker.split(".")[0]
+    ticker=ticker+".NS"
+    stock = yf.Ticker(ticker)
+    df = stock.history(period="1y")
+    df=df[["Close","Volume"]]
+    df.index=[str(x).split()[0] for x in list(df.index)]
+    df.index.rename("Date",inplace=True)
+    df=df[-history:]
+    # print(df.columns)
+    
+    return df.to_string()
+
+# Script to scrap top5 googgle news for given company name
+def google_query(search_term):
+    if "news" not in search_term:
+        search_term=search_term+" stock news"
+    url=f"https://www.google.com/search?q={search_term}&cr=countryIN"
+    url=re.sub(r"\s","+",url)
+    return url
+
+def get_recent_stock_news(company_name):
+    # time.sleep(4) #To avoid rate limit error
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+    g_query=google_query(company_name)
+    res=requests.get(g_query,headers=headers).text
+    soup=BeautifulSoup(res,"html.parser")
+    news=[]
+    for n in soup.find_all("div","n0jPhd ynAwRc tNxQIb nDgy9d"):
+        news.append(n.text)
+    for n in soup.find_all("div","IJl0Z"):
+        news.append(n.text)
+
+    if len(news)>6:
+        news=news[:4]
+    else:
+        news=news
+    news_string=""
+    for i,n in enumerate(news):
+        news_string+=f"{i}. {n}\n"
+    top5_news="Recent News:\n\n"+news_string
+    
+    return top5_news
+
+
+# Fetch financial statements from Yahoo Finance
+def get_financial_statements(ticker):
+    # time.sleep(4) #To avoid rate limit error
+    if "." in ticker:
+        ticker=ticker.split(".")[0]
+    else:
+        ticker=ticker
+    ticker=ticker+".NS"    
+    company = yf.Ticker(ticker)
+    balance_sheet = company.balance_sheet
+    if balance_sheet.shape[1]>=3:
+        balance_sheet=balance_sheet.iloc[:,:3]    # Remove 4th years data
+    balance_sheet=balance_sheet.dropna(how="any")
+    balance_sheet = balance_sheet.to_string()
+    return balance_sheet
+
+
+#Openai function calling
+function=[
+        {
+        "name": "get_company_Stock_ticker",
+        "description": "This will get the indian NSE/BSE stock ticker of the company",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "ticker_symbol": {
+                    "type": "string",
+                    "description": "This is the stock symbol of the company.",
+                },
+
+                "company_name": {
+                    "type": "string",
+                    "description": "This is the name of the company given in query",
+                }
+            },
+            "required": ["company_name","ticker_symbol"],
+        },
+    }
+]
+
+
+def get_stock_ticker(query):
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            messages=[{
+                "role":"user",
+                "content":f"Given the user request, what is the comapany name and the company stock ticker ?: {query}?"
+            }],
+            functions=function,
+            function_call={"name": "get_company_Stock_ticker"},
+    )
+    message = response["choices"][0]["message"]
+    arguments = json.loads(message["function_call"]["arguments"])
+    company_name = arguments["company_name"]
+    company_ticker = arguments["ticker_symbol"]
+    return company_name,company_ticker
+
+def Anazlyze_stock(query):
+    #agent.run(query) Outputs Company name, Ticker
+    Company_name,ticker=get_stock_ticker(query)
+    print({"Query":query,"Company_name":Company_name,"Ticker":ticker})
+    stock_data=get_stock_price(ticker,history=10)
+    stock_financials=get_financial_statements(ticker)
+    stock_news=get_recent_stock_news(Company_name)
+
+    # available_information=f"Stock Price: {stock_data}\n\nStock Financials: {stock_financials}\n\nStock News: {stock_news}"
+    available_information=f"Stock Financials: {stock_financials}\n\nStock News: {stock_news}"
+
+    # print("\n\nAnalyzing.....\n")
+    analysis=llm(f"Give detail stock analysis, Use the available data and provide investment recommendation. \
+             The user is fully aware about the investment risk, dont include any kind of warning like 'It is recommended to conduct further research and analysis or consult with a financial advisor before making an investment decision' in the answer \
+             User question: {query} \
+             You have the following information available about {Company_name}. Write (5-8) pointwise investment analysis to answer user query, At the end conclude with proper explaination.Try to Give positives and negatives  : \
+              {available_information} "
+             )
+    # print(analysis)
+
+    return analysis
